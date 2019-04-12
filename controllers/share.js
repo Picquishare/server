bdeb9d371bba611a93eb84b6f768b398da491fd9
Lalu Oldi Alfitra <oldia9@gmail.com>
2019-04-12 13:25:13 +0700

    add search by tag

--- controllers/share.js
+++ controllers/share.js
@@ -1,4 +1,5 @@
-const Share = require('../models/share')
+const Share = require('../models/share'),
+Tag = require('../models/tags')
 
 class ShareController {
   static getAllSharePic(req, res) {
@@ -12,6 +13,43 @@ class ShareController {
       .catch((err) => {
         res.status(400).json({
           message: err
+        })
+      })
+  }
+
+  static findByTag (req, res) {
+      Tag
+      .findOne({
+          tags: req.body.tags
+      })
+      .populate({
+          path: 'ShareId',
+          populate: {
+              path: 'UserId'
+          }
+      })
+      .then(function (allShare) {
+          res.status(200).json(allShare)
+      })
+      .catch(function (err) {
+          res.status(500).json({
+              message: err
+          })
+      })
+  }
+
+  static recentTag (req, res) {
+      Tag
+      .find({})
+      .sort({
+          createdAt: 'desc'
+      })
+      .then(function (allTags) {
+          res.status(allTags)
+      })
+      .catch(function (err) {
+        res.status(500).json({
+            message: err
         })
       })
   }
